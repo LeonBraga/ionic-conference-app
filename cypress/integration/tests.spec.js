158ceeeb89ce8123a5c8ba9b144949e1c6d5f1fe
UsuarioTeste <leonathanbraga@id.uff.br>
2020-09-26 20:59:23 -0300

    add: new example tests

--- cypress/integration/tests.spec.js
+++ cypress/integration/tests.spec.js
@@ -10,7 +10,6 @@ describe('testes-mobile', () => {
         */
     })
 
-
     it("swipes through tutorial", () => {
         cy.habilitaTutorialEmStorage()
 
@@ -27,33 +26,98 @@ describe('testes-mobile', () => {
         cy.contains('Skip').click()
     });
 
-    // it("adds a session from list to favorites", () => {
-    //     cy.desabilitaTutorialEmStorage();
-    //     cy.contains('University of Ionic').click({ force: true });
-    //     cy.get('[data-cy=favorite]').click();
-    //     cy.get('[data-cy=back').click();
-    //     cy.contains('Favorites').click();
-    //     cy.contains('University of Ionic');
-    // })
+    it("adds a session from list to favorites", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.contains('University of Ionic').click({ force: true });
+
+        cy.get('[data-cy=favorite]').click();
+        cy.get('[data-cy=back]').click();
+
+        cy.contains('Favorites').click();
+        cy.contains('University of Ionic');
+    })
 
     it("enables dark mode", () => {
         cy.desabilitaTutorialEmStorage();
+        // cy.get('ion-menu-button').click();
+        // cy.get('ion-toggle').click();
         cy.get('[data-cy=menu]').click();
         cy.get('[data-cy=dark-mode]').click();
+
 
         cy.contains("Dark Mode")
             .should('have.css', 'color', 'rgb(255, 255, 255)')
     })
 
-    // it("searches for a session and adds to favorites", () => {
-    //     cy.desabilitaTutorialEmStorage();
-    //     cy.get('[data-cy=search]').click();
-    //     cy.get('[data-cy=searchbar]').click().type("Angular{enter}")
-    //     cy.contains("Angular Directives").click();
-    //     cy.get('[data-cy=favorite]').click();
-    //     cy.get('[data-cy=back]').click();
-    //     cy.contains("Favorites").click();
-    //     cy.contains("Angular Directives");
-    // })
+    it("searches for a session and adds to favorites", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.get('[data-cy=search]').click();
+        cy.get('[data-cy=searchbar]').click().type("Angular{enter}")
+
+        cy.contains("Angular Directives").click();
+
+        cy.get('[data-cy=favorite]').click();
+        cy.get('[data-cy=back]').click();
+
+        cy.contains("Favorites").click();
+        cy.contains("Angular Directives");
+    })
+    
+    it("reset schedule filter", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.get('[data-cy=filter]').click();
+        
+        cy.contains("Reset").click();
+        cy.contains("Done").click();
+
+        cy.contains("No Sessions Found").should('exist');
+    })
+
+    //Exemplo de teste perigoso quando em Ionic
+    it("remove track of 'Food' from schedule with filter", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.url().should('include', '/schedule');
+
+        cy.contains("Breakfast").should('be.visible');
+        cy.contains("Lunch");
+
+        cy.get('[data-cy=filter]').click();
+        
+        //cy.contains("Food").click({ force: true }); //Encontra a label que possui "Food" e clica nela mas não clica no Botão Pai que desmarca a checkbox.
+        cy.contains("Food").parent().click();
+        cy.contains("Done").click();
+
+        cy.contains("Breakfast").should('exist').and('not.be.visible');
+        cy.contains("Lunch").should('exist').and('not.be.visible');
+    })
+
+    it("keep track of only 'Food' from schedule with filter", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.url().should('include', '/schedule');
+
+        cy.get('[data-cy=filter]').click();
+        
+        cy.contains("Reset").click();
+        cy.contains("Food").parent().click();
+        cy.contains("Done").click();
+
+        cy.contains("Breakfast").should('be.visible');
+        cy.contains("Lunch").should('be.visible');
+    })
+
+    it("go to the speakers tab", () => {
+        cy.desabilitaTutorialEmStorage();
+
+        cy.get('[data-cy=menu]').click();
+        cy.contains('Speakers').click();
+        cy.get('body').click('right');
+
+        cy.url().should('include', '/speakers');
+    })
 
 })
